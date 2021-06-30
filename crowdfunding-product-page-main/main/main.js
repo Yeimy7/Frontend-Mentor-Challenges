891cc3dd1cfcc6423ed8bdf2d85753a94d97a912
Yeim <brielalica@gmail.com>
2021-06-30 19:22:57 -0400

    Crowdfunding product page challenge finished

--- crowdfunding-product-page-main/main/main.js
+++ crowdfunding-product-page-main/main/main.js
@@ -5,6 +5,16 @@ window.addEventListener('load', () => {
     const close = document.getElementById('close-modal');
     const backModal = document.getElementById('back');
     const modalBig = document.querySelector('.modal-big');
+    const save = document.querySelector('.button-save__icon');
+    let sw = true;
+
+    save.addEventListener('click', () => {
+        document.querySelector('circle').classList.toggle('circle');
+        document.querySelector('path').classList.toggle('path');
+        (sw)?document.querySelector('.button-save__sentence').innerHTML='Bookmarked':document.querySelector('.button-save__sentence').innerHTML='Bookmark';
+        sw=!sw;
+        document.querySelector('.button-save__sentence').classList.toggle('green')
+    })
 
 
     iconTop.addEventListener('click', () => {
@@ -19,18 +29,18 @@ window.addEventListener('load', () => {
         modalBig.classList.add('display')
     });
 
-    
+
     const btnBamboo = document.getElementById('btn-bamboo');
     const btnBlack = document.getElementById('btn-black');
-    const radioBall=document.querySelectorAll('.radio-ball');
-    const radioButton=document.querySelectorAll('.radio-button');
-    const cardInputs=document.querySelectorAll('.card__inputs');
-    const mainCards=document.querySelectorAll('.cardBig');
-    const contin=document.querySelectorAll('.continue');
-    const message=document.getElementById('message');
-    const go=document.getElementById('goIt');
+    const radioBall = document.querySelectorAll('.radio-ball');
+    const radioButton = document.querySelectorAll('.radio-button');
+    const cardInputs = document.querySelectorAll('.card__inputs');
+    const mainCards = document.querySelectorAll('.cardBig');
+    const contin = document.querySelectorAll('.continue');
+    const message = document.getElementById('message');
+    const go = document.getElementById('goIt');
 
-    btnBamboo.addEventListener('click',()=>{
+    btnBamboo.addEventListener('click', () => {
         modalBig.classList.add('display');
         radioBall[1].classList.add('display');
         cardInputs[1].classList.add('display');
@@ -38,26 +48,26 @@ window.addEventListener('load', () => {
         window.scrollTo(0, 300);
     });
 
-    btnBlack.addEventListener('click',()=>{
+    btnBlack.addEventListener('click', () => {
         modalBig.classList.add('display');
         radioBall[2].classList.add('display');
         cardInputs[2].classList.add('display');
         mainCards[2].classList.add('border');
-        window.scrollTo(0,500);
+        window.scrollTo(0, 500);
     })
 
     contin.forEach(e => {
-        e.addEventListener('click',()=>{
+        e.addEventListener('click', () => {
             modalBig.classList.remove('display');
             message.classList.add('display');
         })
     });
-    go.addEventListener('click',()=>{
+    go.addEventListener('click', () => {
         message.classList.remove('display')
         quitar();
     })
-    radioButton.forEach((e,index) => {
-        e.addEventListener('click',()=>{
+    radioButton.forEach((e, index) => {
+        e.addEventListener('click', () => {
             quitar();
             radioBall[index].classList.add('display');
             cardInputs[index].classList.add('display');
@@ -65,7 +75,7 @@ window.addEventListener('load', () => {
         })
     });
 
-    function quitar(){
+    function quitar() {
         radioBall.forEach(e => {
             e.classList.remove('display');
         });
